381494f5fdbf4478741fd5d4fc5e3d58f45d027e
florije1988 <florije1988@gmail.com>
2015-07-03 16:36:00 +0800

    修改为html的东西。

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def page(request):
+    # return HttpResponse("Hello world!")
+    return render(request, 'index.html')
